db5c17bce3b486d45b1d23be38e2664ef80c62c9
Vasiliy Zazdravnykh <coder.vz@gmail.com>
2024-05-18 21:00:23 +0300

    add classes

---
+++ src/logic/APIHelper.ts
@@ -0,0 +1,21 @@
+
+/**
+ * Класс для взаимодействия с API
+ */
+export class APIHelper {
+    constructor() {
+
+    }
+
+    getDomain() {}
+    setDomain() {}
+    getDomains() {}
+    setActivateDomain(domainId: number) {}
+
+
+    getManager(managerId: number) {}
+    setManager() {}
+    getManagers() {}
+    setAcivateManager(is: boolean) {}
+    removeManager(managerId: number) {}
+}
