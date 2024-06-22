d47a0fbdc2d5e732e6ceca5a7e742b019cd981dc
vzx7 <coder.vz@gmail.com>
2024-06-22 16:13:42 +0300

    fix

--- src/components/TableService.tsx
+++ src/components/TableService.tsx
@@ -125,7 +125,7 @@ const TableService = () => {
                           </svg>
                         </button>
                         : !domain.active && currentUser?.role === Role.admin ?
-                          <button title="Активирвать" onClick={onClick} data-isconfigured={domain.isConfigured} data-active={domain.active} data-id={domain.id} className="hover:text-primary">
+                          <button title="Активировать" onClick={onClick} data-isconfigured={domain.isConfigured} data-active={domain.active} data-id={domain.id} className="hover:text-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                               <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                               <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
